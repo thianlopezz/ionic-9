a1e058eac94835698a4bfba7afa92284e6d6056c
Thian Lopez <thianlopezz@gmail.com>
2019-09-28 10:08:44 -0500

    ionic parcial dia 3

---
+++ src/app/services/producto.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ProductoService {
+  constructor(private http: HttpClient) {}
+
+  getProductos() {
+    return this.http.get("http://localhost:9000/productos");
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { ModalController, ToastController } from "@ionic/angular";
 import { ProductoFormComponent } from "../components/producto-form/producto-form.component";
+import { ProductoService } from "../services/producto.service";
 
 @Component({
   selector: "app-tab1",
@@ -8,45 +9,21 @@ import { ProductoFormComponent } from "../components/producto-form/producto-form
   styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
-  productos = [
-    {
-      _id: 1,
-      descripcion: "Computadora",
-      precio: 1200,
-      categoria: "TECNOLOGIA",
-      // feCreacion: new Date(),
-      stock: 4,
-      urlFoto:
-        "https://i2k7m8f5.stackpathcdn.com/7962-large_default/computadora-gamer-i3-7100-essential-de-8gb-1tb.jpg"
-    },
-    {
-      _id: 2,
-      descripcion: "Computadora 2",
-      precio: 1200,
-      categoria: "TECNOLOGIA",
-      // feCreacion: new Date(),
-      stock: 4,
-      urlFoto:
-        "https://i2k7m8f5.stackpathcdn.com/7962-large_default/computadora-gamer-i3-7100-essential-de-8gb-1tb.jpg"
-    },
-    {
-      _id: 3,
-      descripcion: "Computadora 3",
-      precio: 1200,
-      categoria: "TECNOLOGIA",
-      // feCreacion: new Date(),
-      stock: 4,
-      urlFoto:
-        "https://i2k7m8f5.stackpathcdn.com/7962-large_default/computadora-gamer-i3-7100-essential-de-8gb-1tb.jpg"
-    }
-  ];
+  productos = [];
 
   loading = false;
 
   constructor(
     public modalController: ModalController,
-    public toastController: ToastController
+    public toastController: ToastController,
+    private pruductoService: ProductoService
   ) {}
+
+  ionViewWillEnter() {
+    this.pruductoService.getProductos().subscribe((response: any) => {
+      this.productos = response.productos;
+    });
+  }
 
   onProductoClick(params) {
     const { producto, accion } = params;
